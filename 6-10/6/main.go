59262941a1f8b41ac545ca0be0dd45a6d4b4f67c
Apurva <apurva.jpr@gmail.com>
2023-07-20 15:27:35 +0530

    Done with day 6, general code for part-1 gave ans for part-2 also

--- 6-10/6/main.go
+++ 6-10/6/main.go
@@ -24,6 +24,24 @@ func part1() {
   }
 }
 
+func part2() {
+	input, _ := os.ReadFile("input.txt")
+  str := string(input)
+  for i := range str {
+    repeat := 0
+    for i1 := 0; i1 < 14; i1++ {
+      condition := strings.Contains(str[i:i + i1] + str[i + i1 + 1:i+14], string(str[i + i1]))     
+      if condition {
+        repeat++
+      }
+    }
+    if repeat == 0 {
+      fmt.Println(i + 14) 
+      break
+    }
+  }
+}
+
 func main() {
-  part1()
+  part2()
 }
